898138c8ac7f643f5ee732cefb5c5b093ba03a9d
Jaylan Jones <jaylan@DevMountains-MacBook-Pro.local>
2017-12-16 15:55:15 -0600

    First attempt at stage 10 via stage 9, was rough but got to 8 without looking back

--- src/Stage 9/components/EmployeeList.js
+++ src/Stage 9/components/EmployeeList.js
@@ -6,7 +6,10 @@ class EmployeeList extends Component {
       <div>
         <ul className="listContainer">
           {
-            
+            this.props.employees.map((employee)=>{
+              return (<li className="listText "key={employee.id} onClick={()=>{this.props.selectEmployee(employee)}}>{employee.name}</li>
+              )
+            })
           }
         </ul>
       </div>
